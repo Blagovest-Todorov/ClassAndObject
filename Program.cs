b3e9a0388f43a70c28ca8fd28be052c1365d3e86
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-28 10:55:36 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //class -template, model, protorype structure from which me made an instance -called Object
+            //Object is an instance of a given class ;-> creating an object from a class is called instantiation
+            // the Object is the real implem entation of a give class, the real raelisation of the given class
+            //Object has properties, fields, methods, (attributes)
+            //Objects have a state ->set of values associated with their attributes
+
+
+        }
+    }
+}
